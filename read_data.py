86204d08211ad2be4388dc31d88722a667cb0056
Kante Taete <36311041+KanteTaete@users.noreply.github.com>
2026-01-06 15:14:09 +0100

    Refactor configuration management: migrate from environment variables to YAML file for InfluxDB and SUN2000 settings provided by command line argument: - added necessary libraries in requirements - added config file in subfolder "etc" - removed env variables from environment file

--- read_data.py
+++ read_data.py
@@ -14,124 +14,172 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# imports from standard libraries
 import asyncio
 import datetime
 import time
 import os
 import sys
-import atexit
+import atexit                   #to call functions on exit of script
+import yaml                     #to read configuration from yaml file
+import argparse                 #argument parser to parse command line arguments
 
+#imports from 3rd party libraries
 from huawei_solar import AsyncHuaweiSolar
-
 # Lib for InfluxDB v1
-from influxdb import InfluxDBClient
+from influxdb import InfluxDBClient as InfluxDBClient_v1
 # Lib for InfluxDB v2
-import influxdb_client
+from influxdb_client.client.influxdb_client import InfluxDBClient as InfluxDBClient_v2
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
+# derive command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--configfile", 
+                    default = './etc/sun2000.yaml',
+                    help="path to configuration file (default: ./etc/sun2000.yaml)")
+args = parser.parse_args()
+
+# read configuration from args.configfile (default: ./etc/sun2000.yaml)
+try:
+    with open(args.configfile, 'r') as config_file:
+        config = yaml.safe_load(config_file)
+except FileNotFoundError:
+    sys.stderr.write(f'Error: Configuration file "{args.configfile}" not found.\n')
+    exit(1)
+except yaml.YAMLError as e:
+    sys.stderr.write(f'Error: YAML error while reading configuration file "{args.configfile}": {e}\n')
+    exit(1)
+
+
+#***************** function definition start *****************#
+
 def read_secret(variable_name):
+    """read_secret reads secret from a file 
+    if environment variable <variable_name>_FILE is set,
+    otherwise from environment variable <variable_name>.
+
+    Args:
+        variable_name (str): name of environment variable or file name containing the secret
+
+    Returns:
+        str: secret value
+    """
     if f'{variable_name}_FILE' in os.environ:
-        with open(os.environ.get(f'{variable_name}_FILE'), 'r') as secret_file:
+        with open(str(os.environ.get(f'{variable_name}_FILE')), 'r') as secret_file:
             secret = secret_file.read()
     else:
-        secret = os.environ.get(variable_name, None)
+        secret = str(os.environ.get(variable_name, None))
     return secret
 
-INFLUX_DB_VERSION = int(os.environ['INFLUX_DB_VERSION'])
-
-# Variables for InfluxDB version v1
-INFLUX_V1_HOST = os.environ['INFLUX_V1_HOST']
-INFLUX_V1_DB = os.environ['INFLUX_V1_DB']
-INFLUX_V1_USERNAME = os.environ['INFLUX_V1_USERNAME']
-INFLUX_V1_PASSWORD = read_secret('INFLUX_V1_PASSWORD')
-
-# Variables for InfluxDB version v2
-INFLUX_V2_URL = os.environ['INFLUX_V2_URL']
-INFLUX_V2_BUCKET = os.environ['INFLUX_V2_BUCKET']
-INFLUX_V2_ORG = os.environ['INFLUX_V2_ORG']
-INFLUX_V2_TOKEN = os.environ['INFLUX_V2_TOKEN']
-
-
-SUN2000_HOST = os.environ['SUN2000_HOST']
-SUN2000_PORT = int(os.environ['SUN2000_PORT'])
-SUN2000_SLAVE_ID = int(os.environ.get('SUN2000_SLAVE_ID'))
-SUN2000_TIMEOUT = int(os.environ.get('SUN2000_TIMEOUT'))
-
-
-READ_INTERVAL = int(os.environ.get('READ_INTERVAL'))
-
-def on_exit(db_client: influxdb_client.InfluxDBClient, write_api: influxdb_client.InfluxDBClient.write_api):
-    """Close clients after terminate a script.
+def on_exit(db_client, write_api):
+    """on_exit Close clients after terminate a script.
 
     :param db_client: InfluxDB client
-    :param write_api: WriteApi
+    :param write_api: WriteApi  - only for InfluxDB v2
     :return: nothing
     """
-    write_api.close()
+    if write_api is not None:
+        write_api.close()
     db_client.close()
+
+#***************** function definition end *****************#
+
+#***************** configuration variables (constants) definition start *****************#
+
+INFLUX_DB_VERSION = config['influx']['influx_db_version']
+
+# variables for InfluxDB version v1
+INFLUX_V1_HOST = config['influx']['influx_v1_host']
+INFLUX_V1_DB = config['influx']['influx_v1_db']
+INFLUX_V1_USERNAME = config['influx']['influx_v1_username']
+INFLUX_V1_PASSWORD = config['influx']['influx_v1_password']
+
+# variables for InfluxDB version v2
+INFLUX_V2_URL = config['influx']['influx_v2_url']
+INFLUX_V2_BUCKET = config['influx']['influx_v2_bucket']
+INFLUX_V2_ORG = config['influx']['influx_v2_org']
+INFLUX_V2_TOKEN = config['influx']['influx_v2_token']
+
+
+# variables for Huawei SUN2000 inverter
+SUN2000_HOST = config['inverter']['sun2000_host']
+SUN2000_PORT = config['inverter']['sun2000_port']
+SUN2000_SLAVE_ID = config['inverter']['sun2000_slave_id']
+SUN2000_TIMEOUT = config['inverter']['sun2000_timeout']
+SUN2000_REGISTERS = config['inverter']['sun2000_registers']
+
+
+# general variables
+READ_INTERVAL = config['general']['read_interval']
+
+#***************** configuration variables definition end *****************#
 
 
 if INFLUX_DB_VERSION == 1:
-    dbclient = InfluxDBClient(INFLUX_V1_HOST, database=INFLUX_V1_DB,
+    if INFLUX_V1_PASSWORD is None:
+        INFLUX_V1_PASSWORD = read_secret('INFLUX_V1_PASSWORD')
+    dbclient_v1 = InfluxDBClient_v1(INFLUX_V1_HOST, database=INFLUX_V1_DB,
                           username=INFLUX_V1_USERNAME, password=INFLUX_V1_PASSWORD)
 elif INFLUX_DB_VERSION == 2:
-    dbclient = influxdb_client.InfluxDBClient(url=INFLUX_V2_URL, token=INFLUX_V2_TOKEN,org=INFLUX_V2_ORG)
-    write_api=dbclient.write_api(write_options=SYNCHRONOUS)
+    dbclient_v2 = InfluxDBClient_v2(url=INFLUX_V2_URL, token=INFLUX_V2_TOKEN,org=INFLUX_V2_ORG)
+    write_api=dbclient_v2.write_api(write_options=SYNCHRONOUS)
 else:
     sys.stderr.write('Error: No InfluxDB version given in environement variable "INFLUX_DB_VERSION" (file "environment.env").')
     exit(1)
 
 
-async def get_solar_data():
+async def get_solar_data(registers):
+    """get_solar_data connect to inverter, read registers and write data to influxdb
+
+    Args:
+        registers (huawei_solar.register_names): modbus registers to read
+    """
     conn = await AsyncHuaweiSolar.create(host=SUN2000_HOST, port=SUN2000_PORT, slave_id=SUN2000_SLAVE_ID, timeout=SUN2000_TIMEOUT)
     sys.stdout.write(f'Connected to Huawei SUN2000 inverter, host: {SUN2000_HOST}:{SUN2000_PORT} (slave ID: {SUN2000_SLAVE_ID})\n')
-
-    registers = [
-        'active_power',
-        'daily_yield_energy',
-        'accumulated_yield_energy',
-        'internal_temperature',
-        'grid_voltage',
-        'pv_01_voltage',
-        'pv_01_current',
-        'pv_02_voltage',
-        'pv_02_current'
-    ]
+    
     # sleep 2 seconds to ensure connection is established
     time.sleep(2)
 
-    while True:
-        data = []
+    try:
+        while True:
+            data = []
 
-        for r in registers:
-            ms = {}
-            ms["time"] = int(datetime.datetime.now().strftime('%s')) * 10**9
-            result = await conn.get(name=r,slave_id=SUN2000_SLAVE_ID)
+            for r in registers:
+                ms = {}
+                ms["time"] = int(datetime.datetime.now().strftime('%s')) * 10**9
+                result = await conn.get(name=r,slave_id=SUN2000_SLAVE_ID)
 
-            ms["measurement"] = r
-            ms["fields"] = {"value": result.value}
-            ms["tags"] = {"unit": result.unit}
-            sys.stdout.write(f'Read register "{r}": {result.value} {result.unit}\n')
-            if result.value > 0:
-                data.append(ms)
+                ms["measurement"] = r
+                ms["fields"] = {"value": result.value}
+                ms["tags"] = {"unit": result.unit}
+                sys.stdout.write(f'Read register "{r}": {result.value} {result.unit}\n')
+                if result.value > 0:
+                    data.append(ms)
 
-        if INFLUX_DB_VERSION == 1:
-            dbclient.write_points(data)
-        elif INFLUX_DB_VERSION == 2:
-            write_api.write(bucket=INFLUX_V2_BUCKET, org=INFLUX_V2_ORG, record=data)
+            if INFLUX_DB_VERSION == 1:
+                dbclient_v1.write_points(data)
+            elif INFLUX_DB_VERSION == 2:
+                write_api.write(bucket=INFLUX_V2_BUCKET, org=INFLUX_V2_ORG, record=data)
 
-        sys.stdout.write(f'Going to sleep for {READ_INTERVAL} seconds.\n')
-        time.sleep(READ_INTERVAL)
+            sys.stdout.write(f'Going to sleep for {READ_INTERVAL} seconds.\n')
+            time.sleep(READ_INTERVAL)
+    except KeyboardInterrupt:
+        sys.stdout.write('Terminating program on user request (KeyboardInterrupt).\n')
+    except Exception as e:
+        # likely catches all exceptions within while loop --> add extra try/except where reading registers and
+        # where writing to influxdb; implementation pending
+        sys.stderr.write(f'Error: Exception occurred: {e}\n')
+    finally:
+        # close connection to inverter
+        await conn.stop()
+        sys.stdout.write('Connection to Huawei SUN2000 inverter closed.\n')
 
-    # close connection to inverter
-    await conn.stop()
-    sys.stdout.write('Connection to Huawei SUN2000 inverter closed.\n')
-
-asyncio.run(get_solar_data())
+asyncio.run(get_solar_data(SUN2000_REGISTERS))
 
 
-"""
-Call after terminate a script
-"""
-atexit.register(on_exit, dbclient, write_api)
+# call after terminate a script
+if INFLUX_DB_VERSION == 1:
+    atexit.register(on_exit, dbclient_v1, None)
+elif INFLUX_DB_VERSION == 2:
+    atexit.register(on_exit, dbclient_v2, write_api)
